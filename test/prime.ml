c9ace61c4ccb14666acc3b8f710322e179592c42
Xavier Clerc <xclerc@janestreet.com>
2020-02-20 10:00:59 +0000

    v0.14-preview.122.09+157

--- test/prime.ml
+++ test/prime.ml
@@ -5,12 +5,15 @@ module Ml_z_probab_prime = struct
   let%expect_test "print x, probab_prime x" =
     Static.quickcheck
       ~f:(fun x ->
-        List.range 2 7
+        List.range 22 27
         |> List.map ~f:(fun i ->
-          [%message (x : t) (i : int) (Int.equal 0 (probab_prime x i) : bool)])
+          let probab_prime x i = not (Int.equal (Z.probab_prime x i) 0) in
+          [%message (x : t) (i : int) (probab_prime x i : bool)])
         |> Sexp.List)
       ();
-    [%expect {| ((hash c18a1924b9cd5dc81139c67a1b40950d) (uniqueness_rate 85.742188)) |}]
+    [%expect
+      {|
+      ((hash 841bf3c0441e2a8f411a38399b51b11e) (uniqueness_rate 85.742188)) |}]
   ;;
 end
 
